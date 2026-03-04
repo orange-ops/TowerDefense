055c83033d91efa92da100dd6384fe7cc633d51a
Tereza <terka-kerka@seznam.cz>
2026-03-04 12:23:04 +0100

    Enemy follows path from waypoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [SerializeField] private float _movementSpeed = 1f;
+    [SerializeField] private Transform _pathParent;
+    private int _currentWaypoint = 0;
 
+    private void Update()
+    {
+        if (_pathParent != null && _pathParent.childCount > _currentWaypoint)
+        {
+            MoveAlongPath();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveAlongPath()
     {
+        Vector3 targetPosition = _pathParent.GetChild(_currentWaypoint).position;
+        Vector3 movementDirection = (targetPosition - transform.position).normalized;
 
+        transform.position += movementDirection * _movementSpeed * Time.deltaTime;
+
+        if ((targetPosition - transform.position).sqrMagnitude < 0.01f) {
+            _currentWaypoint++;
+        }
     }
 }
