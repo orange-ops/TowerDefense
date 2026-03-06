2233eec7e73860a4a37904f1bd92e56a7333a45d
Tereza <terka-kerka@seznam.cz>
2026-03-06 19:03:34 +0100

    Add PlayerEconomy
    Add PlayerEconomy system for player gold and lives

---
+++ Assets/Scripts/PlayerEconomy.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System;
+
+public class PlayerEconomy : MonoBehaviour
+{
+    [SerializeField] private int _gold = 200;
+    [SerializeField] private int _health= 3;
+
+    //events
+    public event Action<int> OnGoldChanged;
+    public event Action<int> OnHealthChanged;
+    public event Action<int> OnAllHealthLost;
+    
+    public void AddGold(int amount)
+    {
+        _gold += amount;
+        OnGoldChanged?.Invoke(_gold);
+    }
+
+    public bool SpendGold(int amount)
+    {
+        if (_gold >= amount)
+        {
+            _gold -= amount;
+            OnGoldChanged?.Invoke(_gold);
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not enough gold!");
+            return false;
+        }
+    }
+
+    public void LoseHealth(int damage)
+    {
+        if (_health > damage)
+        {
+            _health -= damage;
+            OnHealthChanged?.Invoke(_health);
+        }
+        else
+        {
+            _health = 0;
+            OnAllHealthLost?.Invoke(_health);
+
+        }
+    }
+
+
+}
